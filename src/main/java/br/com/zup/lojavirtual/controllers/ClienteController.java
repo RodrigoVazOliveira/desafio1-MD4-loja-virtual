47a6128328b64dd531c84c00f6c9dc2c92f95b52
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:43:25 -0300

    Criar método para POST para cadastrar o cliente no controller de cliente

--- src/main/java/br/com/zup/lojavirtual/controllers/ClienteController.java
+++ src/main/java/br/com/zup/lojavirtual/controllers/ClienteController.java
@@ -1,13 +1,18 @@
 package br.com.zup.lojavirtual.controllers;
 
+import br.com.zup.lojavirtual.models.Cliente;
 import br.com.zup.lojavirtual.services.ClienteService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes/")
 public class ClienteController {
     private ClienteService clienteService;
 
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return clienteService.cadastrar(cliente);
+    }
 }
