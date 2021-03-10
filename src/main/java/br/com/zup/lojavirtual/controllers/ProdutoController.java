ebd6fddf3a4e4e0303613b1aaf473e9c0d498a84
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:20:39 -0300

    Criar controller de produto

---
+++ src/main/java/br/com/zup/lojavirtual/controllers/ProdutoController.java
@@ -0,0 +1,9 @@
+package br.com.zup.lojavirtual.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos/")
+public class ProdutoController {
+}
