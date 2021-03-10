f1dfef80df8d074d0536e64c30bc4a00a5192384
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:56:29 -0300

    Criar método de pesquisar de cliente por CPF no endpoint clientes/{cpf} no controller de cliente

--- src/main/java/br/com/zup/lojavirtual/controllers/ClienteController.java
+++ src/main/java/br/com/zup/lojavirtual/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.zup.lojavirtual.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/clientes/")
@@ -17,5 +18,15 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente cadastrar(@RequestBody Cliente cliente) {
         return clienteService.cadastrar(cliente);
+    }
+
+    @GetMapping("{cpf}/")
+    @ResponseStatus(HttpStatus.OK)
+    public Cliente pesquisarClientePorCpf(@PathVariable String cpf) {
+        try {
+            return clienteService.pesquisarClientePorCpf(cpf);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
