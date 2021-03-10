8f7dfe94fb22d81e27770353fb6c7fae479ca3b0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 15:18:57 -0300

    Adicionar a anotação para validações de campos de produto e cliente

--- src/main/java/br/com/zup/lojavirtual/models/Cliente.java
+++ src/main/java/br/com/zup/lojavirtual/models/Cliente.java
@@ -1,8 +1,25 @@
 package br.com.zup.lojavirtual.models;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class Cliente {
+
+    @NotNull(message = "O campo nome deve ser fornecido!")
+    @NotEmpty(message = "O nome deve ser informado!")
     private String nome;
+
+    @NotNull(message = "O campo CPF deve ser fornecido!")
+    @NotEmpty(message = "O CPF deve ser informado!")
+    @CPF(message = "O CPF informado é inválido!s")
     private String cpf;
+
+    @NotNull(message = "O campo e-mail deve ser fornecido!")
+    @NotEmpty(message = "O e-mail deve ser informado!")
+    @Email(message = "O e-mail informado é inválido!")
     private String email;
 
     public Cliente() {
--- src/main/java/br/com/zup/lojavirtual/models/Produto.java
+++ src/main/java/br/com/zup/lojavirtual/models/Produto.java
@@ -1,8 +1,20 @@
 package br.com.zup.lojavirtual.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class Produto {
+
+    @NotNull(message = "O nome não foi informado!")
+    @NotEmpty(message = "O nome não foi preenchido!")
     private String nome;
+
+    @NotNull(message = "O preço não foi indicado!")
+    @NotEmpty(message = "O preço deve ser preenchido!")
     private Double preco;
+
+    @Min(value = 1, message = "A quantidade deve ser maior do que zero!")
     private int quantidade;
 
     public Produto() {
