f6e67500a10edec5810c1dc64a1cd80335b436c7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:22:35 -0300

    Criar endpoint produtos/ POST para cadastrar o produto

--- src/main/java/br/com/zup/lojavirtual/controllers/ProdutoController.java
+++ src/main/java/br/com/zup/lojavirtual/controllers/ProdutoController.java
@@ -1,9 +1,21 @@
 package br.com.zup.lojavirtual.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.lojavirtual.models.Produto;
+import br.com.zup.lojavirtual.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produtos/")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto cadastrar(@RequestBody Produto produto) {
+        return produtoService.cadastrar(produto);
+    }
 }
