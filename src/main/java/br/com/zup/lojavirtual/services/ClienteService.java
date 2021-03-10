4e587a22d3b9dd0b4c5caacbcb58ce82ffef2da9
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:39:00 -0300

    Criar serviço de cliente

---
+++ src/main/java/br/com/zup/lojavirtual/services/ClienteService.java
@@ -0,0 +1,10 @@
+package br.com.zup.lojavirtual.services;
+
+import br.com.zup.lojavirtual.models.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteService {
+    private  List<Cliente> clientes = new ArrayList<>();
+}
