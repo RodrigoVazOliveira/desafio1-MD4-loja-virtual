8b17c62898c920a07f383e1f1842ee7a3802f6d7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:19:11 -0300

    Criar método cadastrar no serviço cadastrar produto

--- src/main/java/br/com/zup/lojavirtual/services/ProdutoService.java
+++ src/main/java/br/com/zup/lojavirtual/services/ProdutoService.java
@@ -1,7 +1,18 @@
 package br.com.zup.lojavirtual.services;
 
+import br.com.zup.lojavirtual.models.Produto;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProdutoService {
+
+    private List<Produto> produtos = new ArrayList<>();
+
+    public Produto cadastrar(Produto produto) {
+        produtos.add(produto);
+        return produto;
+    }
 }
