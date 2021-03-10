86441fd688ac16c9da789850256a9e19a1278ac7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 14:39:30 -0300

    Adicionar anotação de serviço do spring boot

--- src/main/java/br/com/zup/lojavirtual/services/ClienteService.java
+++ src/main/java/br/com/zup/lojavirtual/services/ClienteService.java
@@ -1,10 +1,12 @@
 package br.com.zup.lojavirtual.services;
 
 import br.com.zup.lojavirtual.models.Cliente;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ClienteService {
     private  List<Cliente> clientes = new ArrayList<>();
 }
