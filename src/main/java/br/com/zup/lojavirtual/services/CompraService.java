5159ae6f56673db63c430bb13b2c6a1e4687d6ca
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 16:55:42 -0300

    Criar serviço de compra

---
+++ src/main/java/br/com/zup/lojavirtual/services/CompraService.java
@@ -0,0 +1,16 @@
+package br.com.zup.lojavirtual.services;
+
+import br.com.zup.lojavirtual.models.Compra;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CompraService {
+
+    private List<Compra> compras = new ArrayList<>();
+
+    
+
+}
