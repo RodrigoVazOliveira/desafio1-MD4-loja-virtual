5ebf2cba0fe9a8ce9a764d61d862518936e70393
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-10 18:01:54 -0300

    Criar metodo de cadastrar compras com endpoint compras/ POST

--- src/main/java/br/com/zup/lojavirtual/controllers/CompraController.java
+++ src/main/java/br/com/zup/lojavirtual/controllers/CompraController.java
@@ -1,7 +1,12 @@
 package br.com.zup.lojavirtual.controllers;
 
+import br.com.zup.lojavirtual.dtos.CompraDTO;
+import br.com.zup.lojavirtual.models.Cliente;
+import br.com.zup.lojavirtual.models.Compra;
+import br.com.zup.lojavirtual.services.ClienteService;
 import br.com.zup.lojavirtual.services.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ public class CompraController {
 
     @Autowired
     private CompraService compraService;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    public Compra cadastrar(@RequestBody CompraDTO compraDTO) {
+        Cliente cliente = clienteService.pesquisarClientePorCpf(compraDTO.getCpf());
+    }
 }
