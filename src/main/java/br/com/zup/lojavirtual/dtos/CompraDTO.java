f8a3dac846a69ebfb4b4384156fdf1cf66765728
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-11 02:33:35 -0300

    Criar método para converter uma compraDTO para compra no CompraDTO

--- src/main/java/br/com/zup/lojavirtual/dtos/CompraDTO.java
+++ src/main/java/br/com/zup/lojavirtual/dtos/CompraDTO.java
@@ -1,5 +1,9 @@
 package br.com.zup.lojavirtual.dtos;
 
+import br.com.zup.lojavirtual.models.Cliente;
+import br.com.zup.lojavirtual.models.Compra;
+import br.com.zup.lojavirtual.models.Produto;
+
 import java.util.List;
 
 public class CompraDTO {
@@ -24,5 +28,12 @@ public class CompraDTO {
 
     public void setProdutos(List<ProdutoDTO> produtos) {
         this.produtos = produtos;
+    }
+
+    public Compra converterCompraDtoParaCompra(Cliente cliente, List<Produto> produtos) {
+        Compra compra = new Compra();
+        compra.setCliente(cliente);
+        compra.setProdutos(produtos);
+        return compra;
     }
 }
